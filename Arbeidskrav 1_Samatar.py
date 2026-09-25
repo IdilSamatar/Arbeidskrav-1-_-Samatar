09754e990d6cdbc1b5ea12cc932d2f287ef8747b
IdilSamatar <idsam0436@usn.no>
2026-09-25 19:41:59 +0200

    Add files via upload

--- Arbeidskrav 1_Samatar.py
+++ Arbeidskrav 1_Samatar.py
@@ -15,10 +15,13 @@ B = 0.1*10000
 Elbil_Kostnader = (F+T+D+B)
 print (Elbil_Kostnader)
 
+
 F_Bensin = 7500
 T_Bensin = 8.38*365
 D_Bensin = 10000*1
 B_Bensin = 0.3*10000
 Bensin_Kostnader = (F_Bensin+T_Bensin+D_Bensin+B_Bensin )
+print (Bensin_Kostnader)
 
-Kostnadsdifferansen = (Elbil_Kostnader - Bensin_Kostnader)
+Kostnadsdifferansen = (Bensin_Kostnader - Elbil_Kostnader)
+print (Kostnadsdifferansen)
